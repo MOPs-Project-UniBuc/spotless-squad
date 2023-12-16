af149e2bb174cea71aae47995dff2de5da536354
Ioachim Lihor <ioachim.lihor@radcom.com>
2023-12-16 10:13:04 +0200

    chore(infra): update workflows
    Signed-off-by: Ioachim Lihor <ioachim.lihor@radcom.com>

--- spotlesssquad/settings/common.py
+++ spotlesssquad/settings/common.py
@@ -1,19 +1,249 @@
 import sqlalchemy
 
+from spotlesssquad.settings import models
 from spotlesssquad.sql import tables
 
 
-def update_name(email: str, new_name: str, con: sqlalchemy.Connection) -> None:
+def user_exists(email: str, con: sqlalchemy.Connection) -> bool:
+    """
+    Check if user exists.
+    """
+    stmt = sqlalchemy.select(tables.ClientUsers).where(
+        tables.ClientUsers.email == email
+    )
+    result = con.execute(stmt).fetchone()
+    return result is not None
+
+
+def update_user(
+    email: str,
+    values: tuple[sqlalchemy.Column, str],
+    con: sqlalchemy.Connection,
+) -> bool:
     """
     Update user for the given email.
     """
 
+    # check if user exists
+    if not user_exists(email, con):
+        return False
+
     stmt = (
         sqlalchemy.update(tables.ClientUsers)
         .where(tables.ClientUsers.email == email)
-        .values({
-            tables.ClientUsers.name: new_name,
-        })
+        .values(values)
     )
 
-    con.execute(stmt)
+    res = con.execute(stmt)
+
+    return res.rowcount == 1
+
+
+def update_name(
+    email: str,
+    new_name: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.name: new_name},
+        con=con,
+    )
+
+    if res:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
+
+
+def check_password_validity(password: str) -> bool:
+    """
+    Check if password is valid.
+    """
+    return len(password) >= 8
+
+
+def update_password(
+    email: str,
+    new_password: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdatePasswordStatus:
+    """
+    Update user for the given email.
+    """
+
+    if not check_password_validity(new_password):
+        return models.UpdatePasswordStatus.PASSWORD_TOO_SHORT
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.password: new_password},
+        con=con,
+    )
+
+    if res:
+        return models.UpdatePasswordStatus.SUCCESS
+    else:
+        return models.UpdatePasswordStatus.FAILURE
+
+
+def update_phone(
+    email: str,
+    new_phone: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.phone: new_phone},
+        con=con,
+    )
+
+    if res:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
+
+
+def update_address(
+    email: str,
+    new_address: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.address: new_address},
+        con=con,
+    )
+
+    if res:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
+
+
+def update_city(
+    email: str,
+    new_city: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.city: new_city},
+        con=con,
+    )
+
+    if res:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
+
+
+def update_zip(
+    email: str,
+    new_zip: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.zip: new_zip},
+        con=con,
+    )
+
+    if res:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
+
+
+def update_country(
+    email: str,
+    new_country: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.country: new_country},
+        con=con,
+    )
+
+    if res == 1:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
+
+
+def update_imgBase64(
+    email: str,
+    new_imgBase64: str,
+    con: sqlalchemy.Connection,
+) -> models.UpdateStatus:
+    """
+    Update user for the given email.
+    """
+
+    # check if user exists
+    if not user_exists(email, con):
+        return models.UpdateStatus.USER_NOT_FOUND
+
+    res = update_user(
+        email=email,
+        values={tables.ClientUsers.imgBase64: new_imgBase64},
+        con=con,
+    )
+
+    if res:
+        return models.UpdateStatus.SUCCESS
+    else:
+        return models.UpdateStatus.FAILURE
--- spotlesssquad/tests/test_user_update.py
+++ spotlesssquad/tests/test_user_update.py
@@ -1,16 +1,12 @@
 import pandas as pd
 import sqlalchemy
 
-from spotlesssquad.settings.common import update_name
+from spotlesssquad.settings import models
+from spotlesssquad.settings.common import update_name, update_password
 from spotlesssquad.sql import tables
 
 
-def test_update_user_1(sql_engine: sqlalchemy.engine.Engine):
-    """
-    Scenario: Update an user
-    Context: user exists
-    """
-
+def populate_table(sql_engine: sqlalchemy.engine.Engine):
     # INSERT CLIENT USER
     # Populate ClientUsers Table
     users_df = pd.DataFrame(
@@ -18,7 +14,7 @@ def test_update_user_1(sql_engine: sqlalchemy.engine.Engine):
             (
                 "name",
                 "username",
-                "password",
+                "password123",
                 "email@gmail.com",
                 "0767158866",
                 "address",
@@ -48,7 +44,17 @@ def test_update_user_1(sql_engine: sqlalchemy.engine.Engine):
         )
         con.execute(insert_stmt)
 
-        update_name(email="email@gmail.com", new_name="new_user", con=con)
+
+def test_update_user_1(sql_engine: sqlalchemy.engine.Engine):
+    """
+    Scenario: Update an user's name
+    Context: user exists
+    """
+
+    populate_table(sql_engine)
+
+    with sql_engine.begin() as con:
+        res = update_name(email="email@gmail.com", new_name="new_user", con=con)
 
         # Check that the user was updated
         stmt = sqlalchemy.select(tables.ClientUsers).where(
@@ -57,59 +63,88 @@ def test_update_user_1(sql_engine: sqlalchemy.engine.Engine):
 
         users_df = pd.read_sql_query(stmt, con)
 
+    assert res == models.UpdateStatus.SUCCESS
     assert users_df.shape[0] == 1
     assert users_df.iloc[0]["name"] == "new_user"
 
 
 def test_update_user_2(sql_engine: sqlalchemy.engine.Engine):
     """
-    Scenario: Update an user
-    Context: user exists
+    Scenario: Update an user's name
+    Context: user doesn't exist
     """
 
-    # INSERT CLIENT USER
-    # Populate ClientUsers Table
-    users_df = pd.DataFrame(
-        [
-            (
-                "name",
-                "username",
-                "password",
-                "email@gmail.com",
-                "0767158866",
-                "address",
-                "city",
-                "zip",
-                "country",
-                "",
-            ),
-        ],
-        columns=[
-            "name",
-            "username",
-            "password",
-            "email",
-            "phone",
-            "address",
-            "city",
-            "zip",
-            "country",
-            "imgBase64",
-        ],
-    )
+    populate_table(sql_engine)
 
     with sql_engine.begin() as con:
-        insert_stmt = sqlalchemy.insert(tables.ClientUsers).values(
-            users_df.to_dict(orient="records")
-        )
-        con.execute(insert_stmt)
-
-        update_name(email="wrong_email@gmail.com", new_name="new_user", con=con)
+        res = update_name(email="wrong_email@gmail.com", new_name="new_user", con=con)
 
         # Check that the user was updated
         stmt = sqlalchemy.select(tables.ClientUsers).where(
-            (tables.ClientUsers.name == "new_user")
+            (tables.ClientUsers.email == "wrong_email@gmail.com")
         )
         users_df = pd.read_sql_query(stmt, con)
 
+    assert res == models.UpdateStatus.USER_NOT_FOUND
     assert users_df.shape[0] == 0
+
+
+def test_update_user_3(sql_engine: sqlalchemy.engine.Engine):
+    """
+    Scenario: Update an user's password
+    Context: user exists
+    """
+
+    populate_table(sql_engine)
+
+    with sql_engine.begin() as con:
+        res = update_password(
+            email="email@gmail.com", new_password="password1234", con=con
+        )
+
+        # Check that the user was updated
+        stmt = sqlalchemy.select(tables.ClientUsers).where(
+            (tables.ClientUsers.name == "name")
+        )
+        users_df = pd.read_sql_query(stmt, con)
+
+    assert res == models.UpdatePasswordStatus.SUCCESS
+    assert users_df.shape[0] == 1
+    assert users_df.iloc[0]["password"] == "password1234"
+
+
+def test_update_user_4(sql_engine: sqlalchemy.engine.Engine):
+    """
+    Scenario: Update an user's password
+    Context: user doesn't exist
+    """
+
+    populate_table(sql_engine)
+
+    with sql_engine.begin() as con:
+        res = update_password(
+            email="wrong_email@gmail.com", new_password="password123", con=con
+        )
+
+        # Check that the user was updated
+        stmt = sqlalchemy.select(tables.ClientUsers).where(
+            (tables.ClientUsers.email == "wrong_email@gmail.com")
+        )
+        users_df = pd.read_sql_query(stmt, con)
+
+    assert res == models.UpdateStatus.USER_NOT_FOUND
+    assert users_df.shape[0] == 0
+
+
+def test_update_user_5(sql_engine: sqlalchemy.engine.Engine):
+    """
+    Scenario: Update an user's password
+    Context: user exists, password too short
+    """
+
+    populate_table(sql_engine)
+
+    with sql_engine.begin() as con:
+        res = update_password(email="email@gmail.com", new_password="passw", con=con)
+
+    assert res == models.UpdatePasswordStatus.PASSWORD_TOO_SHORT
