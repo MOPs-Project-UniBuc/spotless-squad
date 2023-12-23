d3ceb6712bbae90ebfb83da086603bef56e0f502
Ioachim Lihor <ioachim.lihor@radcom.com>
2023-12-23 17:06:01 +0200

    [SS-63] bugfix(DB): add missing id to tables
    Signed-off-by: Ioachim Lihor <ioachim.lihor@radcom.com>

--- spotlesssquad/app/main.py
+++ spotlesssquad/app/main.py
@@ -1,5 +1,7 @@
 import logging
+import sys
 
+import sentry_sdk
 import streamlit as st
 
 from spotlesssquad.booking import renderer as booking_renderer
@@ -7,6 +9,33 @@ from spotlesssquad.common import get_state
 from spotlesssquad.login import renderer as login_renderer
 from spotlesssquad.my_bookings import renderer as my_bookings_renderer
 from spotlesssquad.settings import renderer as settings_renderer
+
+runner = sys.modules["streamlit.runtime.scriptrunner.script_runner"]
+original_handler = runner.handle_uncaught_app_exception
+
+
+sentry_sdk.init(
+    dsn="https://543eb31bb3cfa5133800e1afad552524@o4506404697210880.ingest.sentry.io/4506404699635712",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+    enable_tracing=True,
+)
+
+
+def sentry_handler(exception: Exception) -> None:
+    """Pass the provided exception through to sentry."""
+    sentry_sdk.capture_exception(exception)
+    return original_handler(exception)
+
+
+if original_handler != sentry_handler:
+    print("---> add exception handler with sentry_handler")
+    runner.handle_uncaught_app_exception = sentry_handler  # type: ignore
 
 
 def init_logging() -> None:
